55e55baf5aadb0af8ea919260c2a065fc7fcdc9a
weiyizhou <weiyi.zhou1514@gmail.com>
2017-03-01 20:24:37 +0000

    Added custom UserManger

--- eduya/eduya/models.py
+++ eduya/eduya/models.py
@@ -1,10 +1,35 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class StudentManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+        
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, password, **extra_fields)
 
 # Create your models here.
-class User(AbstractUser):
-    email = models.EmailField(blank=False)
+"""
+class Student(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(blank=False)    
+    last_name = models.CharField(blank=False)
     password = models.CharField
     tutor = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+"""
