e191ace5b72ff93f4709b9e94b0508254376fb59
Hans Allendorfer <hansallendorfer@gmail.com>
2017-03-01 22:21:30 +0000

    remove urls from students.urls since they are already in the main urls.py file

--- eduya/students/urls.py
+++ eduya/students/urls.py
@@ -2,8 +2,4 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', views.login, name='login'),
-    url(r'^reset/$', views.reset, name='reset'),
-]
+urlpatterns = []
