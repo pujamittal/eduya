476a37c04752ef2c76327ab85ccf1144bd96281e
Hans Allendorfer <hansallendorfer@gmail.com>
2017-03-01 22:15:31 +0000

    added back all urls

--- eduya/eduya/urls.py
+++ eduya/eduya/urls.py
@@ -17,7 +17,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth.views import password_change, password_change_done, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
+from students import views as student_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^students/', include('students.urls'))
+    url(r'^account/password-change/$', password_change, name='password_change'),
+    url(r'^account/password-change/done/$', password_change_done, name='password_change_done'),
+    url(r'^account/password-reset/$', password_reset, name='password_reset'),
+    url(r'^account/password-reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^account/reset-password/confirm/(?P<uid64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
+    url(r'^account/reset/done/$', password_reset_complete, name='password_reset_complete'),
+    url(r'^register/$', student_views.register, {'sign_up': '/templates/sign_up.html'}, name='register'),
+    url(r'^login/$', student_views.login, {'log_in': '/templates/log_in.html'}, name='login'),
+    url(r'^reset/$', student_views.reset, {'pw_reset': '/templates/pw_reset.html'}, name='reset')
 ]
