b90f7eaeeb359cc454a7875a91336f6f18e4a0de
Hans Allendorfer <hansallendorfer@gmail.com>
2017-03-02 23:44:28 +0000

    add post route handlers

--- eduya/posts/views.py
+++ eduya/posts/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from models import Post
 
 # Create your views here.
 def index(request):
-    return HttpResponse("All student postings")
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'posts/index.html', context)
+        
+def post(request, post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+        context = {'post': post}
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+    return render(request, 'posts/detail.html', context)
