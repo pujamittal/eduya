292af7ed20e7d55f35cba999bd8c2b0cf574b380
Tomasz Parzadka <tparzadk@purdue.edu>
2017-03-02 00:27:47 +0000

    Attempted fix

--- eduya/eduya/urls.py
+++ eduya/eduya/urls.py
@@ -15,40 +15,19 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-<<<<<<< HEAD
-=======
-from django.contrib.auth.views import password_change, password_change_done, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 from students import views as student_views
->>>>>>> c576ab931976ad0304130c12a26f08c82102bf4c
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-<<<<<<< HEAD
-    url(r'^account/password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
-    url(r'^account/password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
-    url(r'^account/password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
-    url(r'^account/password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
-    url(r'^account/reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        'django.contrib.auth.views.password_reset_confirm',
-        name='password_reset_confirm'),
-    url(r'^account/reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
-    url(r'^register/$', views.register, {'sign_up': '/templates/sign_up.html'}, name='register'),
-	url(r'^login/$', views.login, name='login'),
-	url(r'^reset/$', view.reset, name='reset'),
-]
-
-=======
     url(r'^$', views.index),
-    url(r'^account/password-change/$', password_change, name='password_change'),
-    url(r'^account/password-change/done/$', password_change_done, name='password_change_done'),
-    url(r'^account/password-reset/$', password_reset, name='password_reset'),
-    url(r'^account/password-reset/done/$', password_reset_done, name='password_reset_done'),
-    url(r'^account/reset-password/confirm/(?P<uid64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
-    url(r'^account/reset/done/$', password_reset_complete, name='password_reset_complete'),
+    url(r'^password-reset/$', password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^reset-password/confirm/(?P<uid64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', password_reset_complete, name='password_reset_complete'),
     url(r'^register/$', student_views.register, name='register'),
     url(r'^login/$', student_views.login, name='login'),
     url(r'^reset/$', student_views.reset, name='reset')
 ]
->>>>>>> c576ab931976ad0304130c12a26f08c82102bf4c
--- eduya/eduya/views.py
+++ eduya/eduya/views.py
@@ -7,37 +7,24 @@
 from django.conf import settings
 from django.core.mail import send_mail
 from django.shortcuts import render
-<<<<<<< HEAD
 from .models import Student 
-
+from django.http import HttpResponse
 
 # Create your views here.
 def register(request):
-    
-    
-#sendmail(subject, message, from_email, to_list, fail_silently=True)
-def conf(request):
-    
-    form = 
-    
-    if form.is_Valid();
-        save_it = form.save(commit=False)
-        save_it.save();
+
+
+def confirm_email(request):
         
         subject = 'Thank you for registering with eduya'
         message = 'Welcome to eduya! Please confirm your email address at the following link.'
         from_email = settings.EMAIL_HOST_USER
-        to_list = [save_it.email, settings.EMAIL_HOST_USER]
+        to_list = [Student.email, settings.EMAIL_HOST_USER]
         send_mail(subject, message, from_email, to_list, fail_silently=True)
         
-        messages.success(request, 'Thank you for registering, please check your email for a confirmation link.')
-        return HttpResponseRedirect('/Thank-you/')
-    return render("thankyou.html", locals(), context_instance=RequestContext(request))
+        return HttpResponseRedirect('/homepage/')
+    return render("homepage.html", locals(), context_instance=RequestContext(request))
 	
-=======
-from django.http import HttpResponse
-
 def index(request):
     # TODO: Link this to index.html/homepage route
     return HttpResponse('Index')
->>>>>>> c576ab931976ad0304130c12a26f08c82102bf4c
