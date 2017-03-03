44d103735970be20f9323caba5ffe88394b6b8a8
weiyizhou <weiyi.zhou1514@gmail.com>
2017-03-03 21:19:40 +0000

    Fixing pw reset

--- eduya/eduya/urls.py
+++ eduya/eduya/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.contrib.auth import views as reset_views
 
 from students import views as student_views
 from home import views as home_views
@@ -23,10 +23,10 @@ from home import views as home_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home_views.index),
-    url(r'^reset-password/$', password_reset, name='password_reset'),
-    url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
-    url(r'^reset-password/confirm/(?P<uid64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset-password-complete/$', password_reset_complete, name='password_reset_complete'),
+    url(r'^reset-password/$', reset_views.password_reset, name='password_reset'),
+    url(r'^reset-password/done/$', reset_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', reset_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset-password-complete/$', reset_views.password_reset_complete, name='password_reset_complete'),
     url(r'^register/$', student_views.registerUser, name='register'),
     url(r'^login/$', student_views.loginUser, name='login'),
     url(r'^logout/$', student_views.logoutUser, name='logout'),
