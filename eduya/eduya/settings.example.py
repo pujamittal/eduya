87e75d909d3840e933bda6f6a4f64ba86691e7cf
Hans Allendorfer <hansallendorfer@gmail.com>
2017-03-31 17:23:51 +0000

    add models

--- eduya/eduya/settings.example.py
+++ eduya/eduya/settings.example.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'posts.apps.PostsConfig',
     'students.apps.StudentsConfig',
+    'courses.apps.CoursesConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
