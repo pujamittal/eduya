1537d23a331b3ece6490bb79894fc6cf1aee78b5
Hans Allendorfer <hansallendorfer@gmail.com>
2017-04-07 17:12:10 +0000

    modify course view route and html page

--- eduya/courses/views.py
+++ eduya/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from courses.models import *
+from students.models import TutorCourse
 import datetime
 import json
 import ast
@@ -22,6 +23,9 @@ def course(request, subject_id, course_id):
     subject = Subject.objects.all().get(abbreviation=subject_id)
     course = Course.objects.all().filter(subject=subject).filter(number=course_id)[0]
     course_sections = CourseSection.objects.all().filter(course=course).order_by('crn')
+    tutors = TutorCourse.objects.all().filter(course=course)
+    professors = ProfessorCourse.objects.all().filter(course=course)
+    teaching_assistants = TeachingAssistantCourse.objects.all().filter(course=course)
 
     for section in course_sections:
         meetings = []
@@ -29,7 +33,7 @@ def course(request, subject_id, course_id):
         for meeting in meetings_json:
             location = meetings_json[meeting]['Room']['BuildingCode'] + meetings_json[meeting]['Room']['RoomNumber']
             section.meetings = {'DaysOfWeek': meetings_json[meeting]['DaysOfWeek'], 'Location': location}
-    context = {'course': course, 'course_sections': course_sections}
+    context = {'course': course, 'course_sections': course_sections, 'tutors': tutors, 'professors': professors, 'teaching_assistants': teaching_assistants}
     return render(request, 'courses/course_profile.html', context)
 
 def section(request, course_id, course_section_id):
