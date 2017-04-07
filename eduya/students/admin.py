13e3bd6587488a209b0de475d28793e05407c65f
Hans Allendorfer <hansallendorfer@gmail.com>
2017-04-07 09:07:30 +0000

    finally have custom admin auth/pages working

--- eduya/students/admin.py
+++ eduya/students/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from students.models import *
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
